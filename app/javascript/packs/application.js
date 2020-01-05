b152799680ab0b87e5d647dabde3ba4316d0e35d
Raimundo Herrera <rjherrera@uc.cl>
2020-01-05 19:11:46 -0300

    feat(linters): rename workflows and clean

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,3 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker');
-const f = (x) => x;
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,4 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
-
-
 end
